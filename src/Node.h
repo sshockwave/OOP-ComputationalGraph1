facac688775d977e1a5d6d486c97a4635906886c
Kerry Su <i_sukai@live.com>
2019-04-10 11:14:46 +0800

    [doc] Node evaluation

--- src/Node.h
+++ src/Node.h
@@ -3,12 +3,41 @@
 #include <memory>
 
 namespace cgraph{
-	class Node;
+	/**
+	 * Node
+	 *
+	 * Basic ideas are similar to
+	 * http://www.deepideas.net/deep-learning-from-scratch-i-computational-graphs/
+	 * Ported to C++.
+	 */
 	class Node{
 		public:
+			/**
+			 * ptr
+			 *
+			 * Garbage collection ported to C++.
+			 * Requires C++ 11.
+			 */
 			typedef std::shared_ptr<Node> ptr;
 			virtual ~Node();
+			/**
+			 * Node::getPrec
+			 *
+			 * Get the list of input nodes.
+			 * For nodes that are not yet calculated,
+			 * the calculation order shall strictly follow
+			 * the order in the vector.
+			 */
 			virtual std::vector<ptr> getPrec() const=0;
+			/**
+			 * Node::eval
+			 *
+			 * Calculate the node's value
+			 * based on input values.
+			 * The order of input values
+			 * should be the same as the order
+			 * given in getPrec.
+			 */
 			virtual float eval(std::vector<float>) const=0;
 	};
 }
