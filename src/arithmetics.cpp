8919d8e8793af26927546c0cd948e6b82475e3d1
Kerry Su <i_sukai@live.com>
2019-04-26 21:41:08 +0800

    [feat] implement add operator

--- src/arithmetics.cpp
+++ src/arithmetics.cpp
@@ -2,6 +2,48 @@
 
 using namespace cgraph;
 
+class NodeAdd:public Node{
+	private:
+		//该节点的值=a+b
+		ptr a,b;
+	public:
+		NodeAdd(ptr _a,ptr _b):a(_a),b(_b){}
+		//返回计算这个节点需要的所有节点
+		std::vector<ptr> getAllPreq()const{
+			return {a,b};
+		}
+		//返回当前需要的而且还没被计算的节点
+		std::vector<ptr> getPreq(Symbol sym)const{
+			//还需要计算的节点放在list中
+			std::vector<ptr>list;
+			//如果a->visited==sym，说明a已经被计算好了
+			if(a->visited!=sym){
+				//a还没被计算好，应该要被计算
+				list.push_back(a);
+			}
+			if(b->visited!=sym){
+				//b同理
+				list.push_back(b);
+			}
+			return list;
+		}
+		void eval(Symbol sym){
+			if(a->version==version&&b->version==version){
+				//这个节点的前驱节点的值没有变化
+				//不需要重新计算
+				return;
+			}
+			//重新计算值
+			value=a->value+b->value;
+			//因为这一次计算中值更新了，版本号也必须跟着更新
+			version=sym;
+		}
+};
+
+cgraph::Expression operator + (cgraph::Expression a,cgraph::Expression b){
+	return static_cast<Node::ptr>(std::make_shared<NodeAdd>(a,b));
+}
+
 class NodePrint:public Node{
 	private:
 		ptr p;
--- src/arithmetics.h
+++ src/arithmetics.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+cgraph::Expression operator + (cgraph::Expression,cgraph::Expression);
+
 cgraph::Expression print(cgraph::Expression,std::string,std::ostream& =std::cout);
 
 //TODO more operators
