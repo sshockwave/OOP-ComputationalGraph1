d3e44754c30ed336bfaa112b45c0751aa546edb5
Kerry Su <me@sshockwave.net>
2019-04-26 23:26:39 +0800

    [fix] correct the name of shared_ptr

--- src/Expression.h
+++ src/Expression.h
@@ -7,7 +7,7 @@ namespace cgraph{
 		private:
 			const Node::ptr ptr;
 		public:
-			template<class T> Expression(std::make_shared<T> p):ptr(p){}
+			template<class T> Expression(std::shared_ptr<T>p):ptr(p){}
 			operator Node::ptr();
 			num_t eval(Symbol=Symbol()) const;
 	};
