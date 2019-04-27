178eebadfc5c1c7e7fa39cc4116c8f9a96aa8674
Kerry Su <i_sukai@live.com>
2019-04-27 16:35:12 +0800

    [feat] add operator api

--- src/arithmetics.h
+++ src/arithmetics.h
@@ -3,8 +3,28 @@
 #include <iostream>
 #include <string>
 
+//10%
 cgraph::Expression operator + (cgraph::Expression,cgraph::Expression);
+cgraph::Expression operator - (cgraph::Expression,cgraph::Expression);
+cgraph::Expression operator * (cgraph::Expression,cgraph::Expression);
+cgraph::Expression operator / (cgraph::Expression,cgraph::Expression);
 
+//3%
+cgraph::Expression sin(cgraph::Expression);
+cgraph::Expression log(cgraph::Expression);
+cgraph::Expression exp(cgraph::Expression);
+cgraph::Expression tanh(cgraph::Expression);
+cgraph::Expression sigmoid(cgraph::Expression);
+
+//3%
 cgraph::Expression print(cgraph::Expression,std::string,std::ostream& =std::cout);
 
-//TODO more operators
+//3%
+cgraph::Expression operator > (cgraph::Expression,cgraph::Expression);
+cgraph::Expression operator < (cgraph::Expression,cgraph::Expression);
+cgraph::Expression operator >= (cgraph::Expression,cgraph::Expression);
+cgraph::Expression operator <= (cgraph::Expression,cgraph::Expression);
+cgraph::Expression operator == (cgraph::Expression,cgraph::Expression);
+
+//3%
+cgraph::Expression cond(cgraph::Expression,cgraph::Expression,cgraph::Expression);
