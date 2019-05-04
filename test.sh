e6d585d5b25a2ae41393add919f8d493b7176704
Kerry Su <i_sukai@live.com>
2019-05-04 10:31:40 +0800

    [chore] abort script abnormally

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ do
 	echo Running test ${file%.*}
 	if dist/main < $file > ${file%.*}.out
 	then echo \\t successfully.
-	else echo \\t Runtime Error! && return 0
+	else echo \\t Runtime Error! && return 1
 	fi
 	if diff ${file%.*}.ans ${file%.*}.out
 	then echo \\t Check passed.
-	else echo \\t Wrong Answer! && return 0
+	else echo \\t Wrong Answer! && return 1
 	fi
 done
