8c8028de65674cc6347b5e150fdd2f7e423bc88a
Kerry Su <i_sukai@live.com>
2019-05-04 11:07:28 +0800

    [chore] fix checker

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
-rm -rf test/*.out
-for file in test/*.in
+rm -rf test/**/*.out
+for file in test/**/*.input
 do
 	echo Running test ${file%.*}
 	if dist/main < $file > ${file%.*}.out
 	then echo \\t successfully.
 	else echo \\t Runtime Error! && return 1
 	fi
-	if diff ${file%.*}.ans ${file%.*}.out
+	if python3 checker.py ${file%.*}.out ${file%.*}.output
 	then echo \\t Check passed.
 	else echo \\t Wrong Answer! && return 1
 	fi
