d80206ae7c20e0fafa7765adb40d60177eac8cdb
Kerry Su <me@sshockwave.net>
2019-06-13 09:24:03 +0800

    [feat] add grad operations

--- Gradient_Node.cpp
+++ Gradient_Node.cpp
@@ -1,4 +1,5 @@
 #include "Gradient_Node.h"
+#include "Constant_Node.h"
 #include <iostream>
 
 Basic_Node* Gradient_Node::EVAL(){
@@ -21,10 +22,30 @@ void Gradient_Node::clear_buffer(){
 }
 
 Gradient_Node::~Gradient_Node(){
-	for(auto it:item){
-		delete it.second;
+	for(auto pii:item){
+		delete pii.second;
 	}
-	for(auto it:abandoned){
-		delete it;
+	for(auto ptr:abandoned){
+		delete ptr;
 	}
+}
+
+Basic_Node* Gradient_Node::get_grad(Basic_Node* var){
+	auto it=item.find(var);
+	if(it==item.end()){
+		auto zero=new Constant_Node(0,"zero");
+		abandoned.push_back(zero);
+		return zero;
+	}
+	return it->second;
+}
+
+void Gradient_Node::push_grad(Basic_Node *target,Basic_Node *grad_val){
+	auto it=item.find(target);
+	if(it==item.end()){
+		item[target]=grad_val;
+		return;
+	}
+	abandoned.push_back(it->second);
+	it->second=new Operation_Plus(it->second,grad_val);
 }
--- Gradient_Node.h
+++ Gradient_Node.h
@@ -12,11 +12,14 @@ class Gradient_Node:Basic_Node{
 		std::vector<Basic_Node*> abandoned;
 		std::map<Basic_Node*,Basic_Node*>item;
 	public:
+		Gradient_Node(Basic_Node*);
 		~Gradient_Node();
 		Basic_Node* EVAL();
 		string get_name();
 		string get_type();
 		void clear_buffer();
+		Basic_Node* get_grad(Basic_Node*);
+		void push_grad(Basic_Node *target,Basic_Node *grad_val);
 };
 
 #endif //GRAD_NODE_H
