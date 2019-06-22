50c5a3d67840716277512f91ad46cb0f2e0b32f9
Kerry Su <me@sshockwave.net>
2019-06-22 08:48:04 +0800

    [feat] add Session

---
+++ Session.cpp
@@ -0,0 +1,30 @@
+#include "Session.h"
+
+using namespace std;
+
+void Session::set(string name,float val){
+	values[name]=val;
+}
+
+void Session::clear(){
+	values.clear();
+}
+
+ostream & operator << (ostream & out,const Session &sess){
+	out<<sess.values.size()<<endl;
+	for(const auto &it:sess.values){
+		out<<it.first<<" "<<it.second<<endl;
+	}
+	return out;
+}
+
+istream & operator >> (istream & in,Session &sess){
+	size_t n;
+	in>>n;
+	for(size_t i=0;i<n;i++){
+		string name;
+		float val;
+		in>>name>>val;
+		sess.set(name,val);
+	}
+}
