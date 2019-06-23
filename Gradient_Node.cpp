c0462e9fe7faccbd6577c86e5bc60b92425cba7a
Kerry Su <me@sshockwave.net>
2019-06-23 20:10:41 +0800

    [doc] add doc for gradient node codes

--- Gradient_Node.cpp
+++ Gradient_Node.cpp
@@ -5,16 +5,17 @@
 #include <queue>
 
 Gradient_Node::Gradient_Node(Basic_Node* target_func,Graph* g){
+	//求所有节点的偏导
 	graph=g;
-	std::map<Basic_Node*,size_t>in_degree;
+	std::map<Basic_Node*,size_t>in_degree;//记录节点的入度
 	std::queue<Basic_Node*>q;
 	q.push(target_func);
-	for(;!q.empty();){
+	for(;!q.empty();){//拓扑排序
 		Basic_Node *x=q.front();
 		q.pop();
 		for(auto next:x->get_preq_nodes()){
 			auto it=in_degree.find(next);
-			if(it==in_degree.end()){
+			if(it==in_degree.end()){//没访问过该节点
 				q.push(next);
 				in_degree[next]=1;
 			}else{
@@ -24,12 +25,14 @@ Gradient_Node::Gradient_Node(Basic_Node* target_func,Graph* g){
 	}
 	q.push(target_func);
 	push_grad(target_func,new Constant_Node(1,"one"));
+	//传播偏导
 	for(;!q.empty();){
 		Basic_Node *x=q.front();
 		q.pop();
 		x->propagate_grad(this);
 		for(auto next:x->get_preq_nodes()){
 			if(--in_degree[next]==0){
+				//该节点的前置节点更新完毕，可以进行传播
 				q.push(next);
 			}
 		}
--- Gradient_Node.h
+++ Gradient_Node.h
@@ -11,15 +11,15 @@ class Gradient_Node: public Basic_Node{
 		std::map<Basic_Node*,Basic_Node*>item;
 		class Graph* graph;
 	public:
-		Gradient_Node(Basic_Node*,Graph*);
+		Gradient_Node(Basic_Node*,Graph*);//求导的目标函数和所属计算图
 		Basic_Node* EVAL();
 		string get_name();
 		string get_type();
 		void clear_buffer();
-		Basic_Node* get_grad(Basic_Node*);
+		Basic_Node* get_grad(Basic_Node*);//获取节点的偏导
 		std::vector<Basic_Node*> get_preq_nodes(){return {};};
-		void push_grad(Basic_Node *target,Basic_Node *grad_val);
-		void add_node(Basic_Node*);
+		void push_grad(Basic_Node *target,Basic_Node *grad_val);//给节点加上偏导值
+		void add_node(Basic_Node*);//将新建的节点加入图的内存管理
 };
 
 #endif //GRAD_NODE_H
